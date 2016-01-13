b6ccb8f5f1c2226115d923b1c752d777266900ed
rbmiao <rbmiao@gmail.com>
2016-01-12 22:10:47 -0500

    downloaded iptables

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -1,6 +1,6 @@
 name 'my_cookbook'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Rongbing Miao'
+maintainer_email 'rbmiao@gmail.com'
 license 'all_rights'
 description 'Installs/Configures my_cookbook'
 long_description 'Installs/Configures my_cookbook'
